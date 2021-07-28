1a628e3eff93059c7e894c6d8df2995fb15ef653
Ashish Kumar <ashish.k@mindfiresolutions.com>
2021-07-28 17:33:01 +0530

    Added int type value change example using pointer

---
+++ pointers/pointer_example1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  currentVal := 12
+  fmt.Println(currentVal)
+  changeVal(&currentVal)
+  fmt.Println(currentVal)
+}
+
+func changeVal(newVal *int) {
+  *newVal = 13;
+}
